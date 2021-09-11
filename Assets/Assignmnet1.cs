5fded36f6d01b6ac11d40ecbf379288c657967d2
MackanMackan <38558616+MackanMackan@users.noreply.github.com>
2021-09-11 18:22:33 +0100

    added loads of features
    background color changing
    paint with diffirent colors
    preview of colors

--- Assets/Assignmnet1.cs
+++ Assets/Assignmnet1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,8 @@ public class Assignmnet1 : ProcessingLite.GP21
 {
     // Start is called before the first frame update
 
-    public Camera camera;
+    public GameObject sprite;
+    SpriteRenderer background;
 
     bool mouseButtonPressedOnce = false;
     bool inPaintMode = false;
@@ -18,6 +20,38 @@ public class Assignmnet1 : ProcessingLite.GP21
 
     float count;
 
+    [Range(1,255)]
+    public int fillColorR;
+    [Range(1, 255)]
+    public int fillColorG;
+    [Range(1, 255)]
+    public int fillColorB;
+    
+    [Range(1, 255)]
+    public int strokeColorR;
+    [Range(1, 255)]
+    public int strokeColorG;
+    [Range(1, 255)]
+    public int strokeColorB;
+
+    [Range(1, 255)]
+    public int backgroundColorR;
+    [Range(1, 255)]
+    public int backgroundColorG;
+    [Range(1, 255)]
+    public int backgroundColorB;
+
+    int previousBackgroundColorR = 0;
+    int previousBackgroundColorB = 0;
+    int previousBackgroundColorG = 0;
+
+    int previousStrokeColorR = 0;
+    int previousStrokeColorG = 0;
+    int previousStrokeColorB = 0;
+
+    int previousFillColorR = 0;
+    int previousFillColorG = 0;
+    int previousFillColorB = 0;
     void Start()
     {
         count = 0;
@@ -26,14 +60,11 @@ public class Assignmnet1 : ProcessingLite.GP21
 
         mousePos = new Vector3(0,0,0);
 
-        Line(4, 7, 4, 3);
-        Line(4, 7, 5, 5);
-        Line(6, 7, 5, 5);
-        Line(6, 7, 6, 3);
+        background = sprite.GetComponent<SpriteRenderer>();
 
-        Line(8, 7, 7, 3);
-        Line(8, 7, 9, 3);
-        Line(7.5f , 5, 8.5f, 5);
+        letterM(-3,0);
+        letterA(-4,0);
+
 
     }
 
@@ -64,11 +95,19 @@ public class Assignmnet1 : ProcessingLite.GP21
         {
             paintLines();
         }
+
+        if (!inEraseMode)
+        {
+            Fill(fillColorR, fillColorG, fillColorB);
+            Stroke(strokeColorR, strokeColorG, strokeColorB);
+        }
+
+        changeBackgroundColor();
+        previewColorStroke();
+        previewColorFill();
     }
     private void paintLines()
     {
-        Fill(255, 255, 255);
-        Stroke(255, 255, 255);
         // takes the mouse position and saves it for the first point used for the LINE method
         if (Input.GetMouseButtonDown(0) && mouseButtonPressedOnce == false)
         {
@@ -89,8 +128,7 @@ public class Assignmnet1 : ProcessingLite.GP21
     }
     private void paint()
     {
-        Fill(255, 255, 255);
-        Stroke(255, 255, 255);
+        
         if (Input.GetMouseButton(0) && count == 0)
         {
             mousePos = Input.mousePosition;
@@ -120,9 +158,17 @@ public class Assignmnet1 : ProcessingLite.GP21
     }
     private void erase()
     {
+        
+        // it takes the background color in real time, if the background would change in runtime
+
+        int r = (int)Math.Round(background.color.r);
+        int g = (int)Math.Round(background.color.g);
+        int b = (int)Math.Round(background.color.b);
+
         // paints a circle filled and outlines of the cameras background color at mouse position, which makes a eraser effect
-        Fill(55, 98, 164);
-        Stroke(55, 98, 164);
+        
+        Fill(r, g, b);
+        Stroke(r, g, b);
 
         if (Input.GetMouseButton(0))
         {
@@ -132,5 +178,60 @@ public class Assignmnet1 : ProcessingLite.GP21
             Circle(worldPosFirstClick.x,worldPosFirstClick.y,1);
 
         }
+    }
+    public void letterA(float x, float y)
+    {
+        Line(8+x, 7+y, 7+x, 3+y);
+        Line(8+x, 7+y, 9+x, 3+y);
+        Line(7.5f+x, 5+y, 8.5f+x, 5+y);
+    }
+    public void letterM(float x, float y)
+    {
+        Line(4+x, 7+y, 4+x, 3+y);
+        Line(4+x, 7+y, 5+x, 5+y);
+        Line(6+x, 7+y, 5+x, 5+y);
+        Line(6+x, 7+y, 6+x, 3+y);
+    }
+    public void previewColorStroke()
+    {
+        if (previousStrokeColorR != strokeColorR || previousStrokeColorG != strokeColorG
+            || previousStrokeColorB != strokeColorB)
+        {
+            Line(0.5f, 9, 0.5f, 7);
+
+            previousStrokeColorR = strokeColorR;
+            previousStrokeColorG = strokeColorG;
+            previousStrokeColorB = strokeColorB;
+        }
+    }
+    public void previewColorFill()
+    {
+        if (previousFillColorR != fillColorR || previousFillColorG != fillColorG
+            || previousFillColorB != fillColorB)
+        {
+            Circle(0.5f, 6, 0.8f);
+
+            previousFillColorR = fillColorR;
+            previousFillColorG = fillColorG;
+            previousFillColorB = fillColorB;
+        }
+    }
+    public void changeBackgroundColor()
+    {
+        if (previousBackgroundColorR != backgroundColorR || previousBackgroundColorG != backgroundColorG 
+            || previousBackgroundColorB != backgroundColorB)
+        {
+            //Changes backcolor in real time
+            background.color = new Color32((byte)backgroundColorR, (byte)backgroundColorG, (byte)backgroundColorB, 255);
+            
+
+            previousBackgroundColorR = backgroundColorR;
+            previousBackgroundColorG = backgroundColorG;
+            previousBackgroundColorB = backgroundColorB;
+
+            sprite.transform.position = new Vector3(sprite.transform.position.x, sprite.transform.position.y, -9.0f);
+            sprite.transform.position = new Vector3(sprite.transform.position.x, sprite.transform.position.y, -9.99f);
+        }
+
     }
 }
