eca9c2371501d900b6519098d5f8c9978922ff4a
MackanMackan <38558616+MackanMackan@users.noreply.github.com>
2021-09-20 15:39:30 +0100

    Improved assignment 4
    added deacceleration to character

--- Assets/Assignment4.cs
+++ Assets/Assignment4.cs
@@ -8,11 +8,14 @@ public class Assignment4 : ProcessingLite.GP21
     private float speed;
     public float baseSpeed;
     public float acceleration;
+    public float deacceleration;
     public float maxSpeed = 15;
     Vector2 characterPos = new Vector2(1, 1);
     Vector2 character2Pos = new Vector2(1, 1);
+    Vector2 continousDirection = new Vector2(0, 0);
     void Start()
     {
+        //character2Pos = new Vector2(Width / 2 , Height / 2);
         speed = baseSpeed;
     }
 
@@ -21,7 +24,7 @@ public class Assignment4 : ProcessingLite.GP21
     {
         Background(0);
         moveCharacter();
-        moveOtherCharacter();
+       // moveOtherCharacter();
     }
     public void moveOtherCharacter()
     {
@@ -36,7 +39,8 @@ public class Assignment4 : ProcessingLite.GP21
     public void moveCharacter()
     {
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        if (input.magnitude != 0)
+        input.Normalize();
+        if (input.magnitude != 0 && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
             character2Pos.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
             character2Pos.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
@@ -46,10 +50,19 @@ public class Assignment4 : ProcessingLite.GP21
             {
                 speed =  maxSpeed;
             }
+            continousDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            continousDirection.Normalize();
+        }
+        else if(character2Pos.magnitude == 0)
+        {
+           speed = baseSpeed;
         }
         else
         {
-            speed = baseSpeed;
+            speed -= deacceleration * Time.deltaTime;
+            speed = Mathf.Clamp(speed, 0, 20);
+            character2Pos.x += continousDirection.x * speed * Time.deltaTime;
+            character2Pos.y += continousDirection.y * speed * Time.deltaTime;
         }
         Circle(character2Pos.x, character2Pos.y, 0.5f);
     }
